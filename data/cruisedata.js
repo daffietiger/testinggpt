ee490438598f4bd5709efff5f5bb4f6cc1ac7959
daffietiger <145070461+daffietiger@users.noreply.github.com>
2023-09-14 16:45:53 -0500

    Add files via upload

---
+++ data/cruisedata.js
@@ -0,0 +1,24 @@
+const cruisedata = {
+  {
+    id: '1',
+    companyLogo: "/images/cruiselogos/royalcaribbean.png",
+    routeName: 'Bahamas Cruise',
+    routeDuration: "5 Days",
+    lowestPrice: "$500",
+    shipName: "Oasis of the Seas",
+    dates: {
+      dateOne: "January 2024",
+      dateTwo: "February 2024",
+      dateThree: "March 2024",
+      dateFour: "April 2024",
+      dateFive: "May 2024",
+      dateSix: "June 2024",
+      dateSeven: "July 2024",
+      dateEight: "August 2024",
+      dateNine: "September 2024",
+      dateTen: "October 2024",
+    }
+    shipImage: "./images/",
+    
+  }
+};
